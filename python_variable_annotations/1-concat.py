41a7315e7cf4b73e773eca8cc4ef9ad4fbae6647
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-07-30 19:09:02 +0300

    Add type annotation function that return cocatenate of tow string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Module that  takes a string str1 and a string str2 as arguments"""
+
+def concat(str1: str, str2: str) -> str:
+"""Return the cocatenate of string"""
+	return str1 + str2
