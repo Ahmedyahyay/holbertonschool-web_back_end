3d0a9254052c140b466435e5ec4843cbb5a6d3c9
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-07-30 18:52:16 +0300

    fix the documentation of the code

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Ad tow float numbers"""
+"""Module that provides a function to add two floating-point numbers."""
 
 
 def add(a: float, b: float) -> float:
-""" Return the sum of two numbers as a float"""
+    """Return the sum of two floating-point numbers a and b."""
     return a + b
