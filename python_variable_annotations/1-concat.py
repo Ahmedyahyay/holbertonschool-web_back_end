91b8f88e2208ad1ca8f2d90bd9bfd0c653964af4
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-07-30 19:16:33 +0300

    Modify 1-concat file

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,5 +2,5 @@
 """ Module that  takes a string str1 and a string str2 as arguments"""
 
 def concat(str1: str, str2: str) -> str:
-"""Return the cocatenate of string"""
+	"""Return the cocatenation of string"""
 	return str1 + str2
