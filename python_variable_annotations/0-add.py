bfb3caa015b11623d9dcf0469407d70a67dd063b
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-07-30 18:51:12 +0300

    fix the documentation of the code

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
+""" Ad tow float numbers"""
 
-
-"""
-Ad tow float numbers
-"""
 
 def add(a: float, b: float) -> float:
-
-
-""" Return the sum of two numbers as a float
-"""
-
-
+""" Return the sum of two numbers as a float"""
     return a + b
