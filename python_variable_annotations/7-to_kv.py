cda9865a5593722bfbd11a6db82436d256f2f6f9
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-07-31 19:42:38 +0300

    Add function to return tuple with key and squared value

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that provides function returning tuple with key and squared value."""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return tuple, the first element is k, second is v squared as float."""
+    return (k, float(v ** 2))
