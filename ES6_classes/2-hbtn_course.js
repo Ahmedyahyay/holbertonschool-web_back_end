25305c3cb04cdef9e290e6af7b03171859e274f7
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-08-23 00:41:24 +0300

    Add task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,26 @@
+class HolbertonCourse{
+    constructor(name, length, students){
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name(){
+        return this._name;
+    }
+    get length(){
+        return this._length;
+    }
+    get students(){
+        return this._students;
+    }
+    set name(value){
+        this._name = value;
+    }
+    set length(value){
+        this._length = value;
+    }
+    set students(value){
+        this._students = value;
+    }
+}
+
