6075b3ccb5a1268e17e65f99a519076e436b1456
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-08-30 22:15:35 +0300

    Add function that returns a new ArrayBuffer with an Int8 value at a specific position

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,7 +5,7 @@ function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
   int8[position] = value;
-  return buffer;
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
